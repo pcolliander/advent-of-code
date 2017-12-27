b66f39a04b6298be67e4d5b16c89a3a26d8e0caf
pcolliander <pontus.colliander@gmail.com>
2017-12-27 08:43:07 +0100

    removed unecessary files

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to ad-of-code
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- test/ad_of_code/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns ad-of-code.core-test
-  (:require [clojure.test :refer :all]
-            [ad-of-code.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
