0b6ba616a75962e1546db1d38ce8be7a17ce6a54
pcolliander <pontus.colliander@gmail.com>
2018-01-06 08:48:45 +0100

    refactored day 7 to use slurp and recursion instead of an atom for state

---
+++ src/ad_of_code/07/core.clj
@@ -0,0 +1,36 @@
+(ns ad-of-code.07.core
+  (:require [clojure.string :as str]))
+
+(defn- get-head [string]
+  (first (str/split string #" ")))
+
+(defn- get-links [string]
+  (let [links-as-string (str/replace (re-find #"->.*$" string)  "->" "")]
+    (-> links-as-string
+        (str/trim)
+        (str/split #", "))))
+
+(defn- map-input [input-lines]
+  (let [string-with-links (filter #(> (.indexOf % ",") 0) input-lines)]
+    (loop [string-with-links string-with-links 
+           n (count string-with-links) 
+           top-programs {}]
+
+      (if (= n 0)
+        top-programs
+        (recur 
+          string-with-links 
+          (dec n) 
+          (conj top-programs (hash-map (get-head (nth string-with-links (dec n) )) (get-links (nth string-with-links (dec n) )))))))))
+
+(defn find-topmost-program [input-path]
+  (let [file (slurp input-path)
+        input-lines (str/split-lines file)
+        top-programs (map-input input-lines)]
+
+    (for [k (keys top-programs)]
+      (if (nil? (some #(when (= % k) %) (flatten (vals top-programs))))
+        (println "result is: " k)) )))
+
+(find-topmost-program "./src/ad_of_code/07/puzzle-input.txt")
+
