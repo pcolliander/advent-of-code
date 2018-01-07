4a35ef896a7690d8f1af0a3205b9036cfa37f0c4
pcolliander <pontus.colliander@gmail.com>
2018-01-07 14:51:43 +0100

    fixed indentation

--- src/ad_of_code/04/core.clj
+++ src/ad_of_code/04/core.clj
@@ -5,16 +5,14 @@
 (defn calc-correct-passwords [input-path]
   (let [file (slurp input-path)
         input-lines (str/split-lines file)]
-
-  (loop [counter 0
-         input-lines input-lines]
-
-    (if-let [line (first input-lines)]
-      (let [words (str/split line #" ")]
-        (recur
-          (if (apply distinct? words) (inc counter) counter)
-          (rest input-lines)))
-      counter))))
+    (loop [counter 0
+           input-lines input-lines]
+      (if-let [line (first input-lines)]
+        (let [words (str/split line #" ")]
+          (recur
+            (if (apply distinct? words) (inc counter) counter)
+            (rest input-lines)))
+        counter))))
 
 (calc-correct-passwords "./src/ad_of_code/04/puzzle-input.txt")
 
@@ -27,16 +25,14 @@
 (defn calc-correct-passwords-anagram [input-path]
   (let [file (slurp input-path)
         input-lines (str/split-lines file)]
-
-  (loop [counter 0
-         input-lines input-lines]
-
-    (if-let [line (first input-lines)]
-      (let [sorted-words (sort-words line)]
-        (recur
-          (if (apply distinct? sorted-words) (inc counter) counter)
-          (rest input-lines)))
-      counter))))
+    (loop [counter 0
+           input-lines input-lines]
+      (if-let [line (first input-lines)]
+        (let [sorted-words (sort-words line)]
+          (recur
+            (if (apply distinct? sorted-words) (inc counter) counter)
+            (rest input-lines)))
+        counter))))
 
 (calc-correct-passwords-anagram "./src/ad_of_code/04/puzzle-input.txt")
 
