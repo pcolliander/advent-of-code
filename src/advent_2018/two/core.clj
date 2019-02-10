371d0b87adf8beacb7b97f73310960363cc557e1
pcolliander <pontus.colliander@gmail.com>
2019-02-10 09:40:08 -0300

    refactored to use map to compare two collections

--- src/advent_2018/two/core.clj
+++ src/advent_2018/two/core.clj
@@ -19,43 +19,31 @@
 ;; (part-one "./src/advent_2018/two/test-input.txt")
 (part-one "./src/advent_2018/two/input.txt")
 
-(defn- get-value-with-diff-one
-  ([head values]
-   
-   (get-value-with-diff-one values head values 0))
-
-  ([original-vals head values diff]
-    (if-let [c (first head)]
-      (if (= c (first values))
-        (recur original-vals (rest head) (rest values) diff)
-        (if (zero? diff)
-          (recur original-vals (rest head) (rest values) (inc diff))
-          nil))
-        original-vals)))
+(defn- get-value-with-diff-one [head right]
+  (let [diff-by-one? (= 1 (count (filter false? (map = head right))))]
+    (if diff-by-one?
+      right)))
 
 (defn- get-char-to-remove-from-head [left right]
-  (if-let [[head & tail] left]
-    (if-not (= head (first right))
-      head
-      (recur tail (rest right)))))
+  (let [comparisons (map (fn [l r]
+                           (if (not= l r) (str l)))
+                         left right)]
 
+  (->> comparisons
+    (some #(when (not (nil? %)) %)))
 
 (defn part-two [input-path]
-  (let [values (->> (read-input input-path)
-                  (map #(s/split % #"")))]
-    
-    (loop [values values]
-      (if-let [[head & tail] values]
-        (let [result (->> tail
-                          (map (partial get-value-with-diff-one head))
-                          (filter #(not (nil? %)))
-                          (first))]
+  (loop [values (read-input input-path)]
+    (if-let [[head & tail] values]
+      (let [result (->> tail
+                    (map (partial get-value-with-diff-one head))
+                    (some #(when (not (nil? %)) %)))]
 
-          (if-not (empty? result)
-            (let [char-to-remove (get-char-to-remove-from-head head result)]
-              (s/replace-first (s/join head) (re-pattern char-to-remove) ""))
-            (recur tail)))))))
+        (if-not (nil? result)
+          (let [char-to-remove (get-char-to-remove-from-head head result)]
+            (s/replace-first head (re-pattern char-to-remove) ""))
+          (recur tail))))))
 
-;; (part-two "./src/advent_2018/two/test-input2.txt")
+(part-two "./src/advent_2018/two/test-input2.txt")
 (part-two "./src/advent_2018/two/input.txt")
 
