c3c9232c0d8bb59edf5aadc0401ab1041fd20b2a
pcolliander <pontus.colliander@gmail.com>
2022-02-04 12:24:04 +0000

    added missing length of operator packets to get correct answers

--- src/2021/day16/core.clj
+++ src/2021/day16/core.clj
@@ -30,15 +30,6 @@
 
 (defn- literal? [id] (= "100" id))
 (def operator? (complement literal?))   
-
-;; (defn- subpackets-in-bit-size [subpackets]
-;;   (let [literal-value-bits (map (fn [values]
-;;                                   (->> values
-;;                                        (map count)
-;;                                        (reduce +)))
-;;                                 subpackets)
-;;         header-bits (* 6 (count subpackets))]
-;;     (reduce + (flatten [literal-value-bits header-bits]))))
              
 (defn- extract-literal [package]
   (loop [package package
@@ -57,39 +48,35 @@
         length-type (string/join head)]
     [(Long/parseLong length-type 2) tail]))
 
+(Long/parseLong "00000000010" 2)
+(Long/parseLong "000000000010110" 2)
+
 (defn- parse-packet [binary]
   (let [[header binary-tail] (split-at 6 binary)
         version (->> header (take 3) string/join)
-        type-id (->> header (take-last 3) string/join)]
-
-    (println :version version)
-    (println :binary binary)
-    (println :type-id type-id)
-
+        type-id (->> header (take-last 3) string/join)
+        header-size (count header)]
     (cond
       (literal? type-id) (let [[literal rest-of-binary] (extract-literal binary-tail)]      
-                           (println :literal/literal literal)
                            {:version-number version
+                            :version-sum (Long/parseLong version 2)
                             :literal literal
                             :len (reduce
                                    size-of-literal
-                                   (count header)
+                                   header-size
                                    literal)})
 
       (operator? type-id) (let [[length-type-id & operator-tail] binary-tail]
-                            (println :length-type-id length-type-id)
                             (case length-type-id
                               0 (let [[length-type-total remaining] (get-length-type operator-tail 15)]
                                    (loop [binary remaining
                                           subpackets []]
-                                     (println :operator/length-type-total length-type-total)
-                                     (println :operator/subpackets subpackets)
-
                                      (let [parsed-length (reduce + (map :len subpackets))]
                                        (if (= length-type-total parsed-length)
                                          {:version version
-                                          :subpackets subpackets
-                                          :len parsed-length}
+                                          :version-sum (+ (Long/parseLong version 2) (apply + (map :version-sum subpackets)))
+                                          :len (+ header-size 16 parsed-length)
+                                          :subpackets subpackets}
                                          (let [parsed (parse-packet binary)
                                                [_ rest-of-binary] (split-at (:len parsed) binary)]
                                            (recur
@@ -99,28 +86,34 @@
                               1 (let [[length-type-total remaining] (get-length-type operator-tail 11)]
                                    (loop [binary remaining
                                           subpackets []]
-                                     (println :subpackets subpackets)
-                                     (println :operator/length-type-total length-type-total)
-                                     (println :operator/subpackets subpackets)
-
                                      (let [parsed-length (reduce + (map :len subpackets))]
                                        (if (= length-type-total (count subpackets))
                                          {:version version
-                                          :subpackets subpackets
-                                          :len parsed-length}
+                                          :version-sum (+ (Long/parseLong version 2) (apply + (map :version-sum subpackets)))
+                                          :len (+ header-size 12 parsed-length)
+                                          :subpackets subpackets}
                                          (let [parsed (parse-packet binary)
                                                [_ rest-of-binary] (split-at (:len parsed) binary)]
                                            (recur rest-of-binary (conj subpackets parsed))))))))))))
 
+(Long/parseLong "000" 2)
+(Long/parseLong "011" 2)
 (defn part-one
   ([] (part-one (slurp "./src/2021/day16/input.txt")))
   ([input]
-   (let [binary (parse input)]
-     (parse-packet binary))))
+   (let [binary (parse input)
+         result (parse-packet binary)]
+
+     (:version-sum result))))
 
 (comment
-;; (extract-literal "101111111000101000")
 (parse "D2FE28")
+(parse "620080001611562C8802118E34")
+
 (part-one "D2FE28")
 (part-one "38006F45291200")
-(part-one "EE00D40C823060"))
+(part-one "EE00D40C823060")
+(part-one "8A004A801A8002F478")
+(part-one "620080001611562C8802118E34")
+(part-one "C0015000016115A2E0802F182340")
+(part-one "A0016C880162017C3686B18A3D4780"))
