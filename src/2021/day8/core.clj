0ce9549f5884ded3e2894c64b7c1a75a6cc8938a
pcolliander <pontus.colliander@gmail.com>
2022-02-14 15:21:36 +0100

    solved part-one

--- src/2021/day8/core.clj
+++ src/2021/day8/core.clj
@@ -3,22 +3,35 @@
              [clojure.string :as string]
              [clojure.pprint :as pp]))
 
-(def example "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf")
+(def example "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+              edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+              fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+              fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+              aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+              fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+              dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+              bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+              egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+              gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce")
 
-(defn- parse [input-path]
-  (->> (string/split (slurp input-path) #",")
+(defn- parse [input]
+  (->> (string/split-lines input)
        (map string/trim)
-       (map #(Integer/parseInt %))))
-
+       (map #(string/split % #" \| "))))
 
 (defn part-one
   ([] (part-one (slurp "./src/2021/day8/input.txt")))
-  ([input-path]
-   (let [positions (parse input-path)
-         costs (for [align-to (range (apply min positions) (inc (apply max positions)))]
-                 (cost positions align-to))]
-
-     (apply min costs))))
+  ([input]
+   (let [values (parse input)]
+     (->> values
+          (map (fn [[_ output-values]]
+                    (->> (string/split output-values #" ")
+                         (map count)
+                         (filter (fn [output-value]
+                                   (some #{output-value} #{2 4 3 7}))))))
+          (map #(reduce (fn [acc n]
+                          (inc acc)) 0 %))
+          (reduce +)))))
 
 (comment
 (part-one example)
