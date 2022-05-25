de9aba06f5901ce7fcb7c9debec539b30903a515
pcolliander <pontus.colliander@gmail.com>
2022-05-25 14:41:47 +0100

    cleanup 2018/day1

--- src/2018/day1/core.clj
+++ src/2018/day1/core.clj
@@ -1,31 +1,30 @@
-(ns advent_2018.one.core
+(ns advent-of-code-2018.core
   (:require [clojure.edn :as edn]
             [clojure.string :as s]))
 
-(defn- read-input [input-path]
-  (->> (s/split-lines (slurp input-path))
+(defn- parse [input]
+  (->> (s/split-lines input)
        (map edn/read-string)))
 
-(defn part-one [input-path]
-  (reduce + (read-input input-path)))
-
-;; (part-one "./src/one/test-input.txt")
-(part-one "./src/advent_2018/one/input2.txt")
+(defn part-one
+  ([] (part-one (slurp "./src/2018/day1/input2.txt")))
+  ([input]
+   (reduce + (parse input))))
 
 (defn part-two
-  ([input-path]
-   (part-two (take 150000 (cycle (read-input input-path))) #{} 0))
-
+  ([] (part-two (slurp "./src/2018/day1/input.txt")))
+  ([input]
+   (part-two (take 150000 (cycle (parse input))) #{} 0))
   ([values seen current-freq]
-   (if-let [[head & tail] values]
-     (let [new-freq (+ head current-freq)]
-       (if (contains? seen new-freq)
-         new-freq
-         (recur
-           tail
-           (conj seen new-freq)
-           new-freq))))))
+    (if-let [[head & tail] values]
+      (let [new-freq (+ head current-freq)]
+        (if (contains? seen new-freq)
+          new-freq
+          (recur
+            tail
+            (conj seen new-freq)
+            new-freq))))))
 
-;; (part-two "./src/one/test-input.txt")
-(part-two "./src/advent_2018/one/input2.txt")
-
+(comment
+  (part-one)
+  (part-two))
