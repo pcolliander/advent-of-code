42c7d7f83a2d4ccc7fc792211e918ee86123edf5
pcolliander <pontus.colliander@gmail.com>
2022-05-28 15:01:46 +0100

    refactored solution a bit

--- src/2018/day2/core.clj
+++ src/2018/day2/core.clj
@@ -2,43 +2,48 @@
   (:require [clojure.string :as s]
             [clojure.set :as cs]))
 
-(defn- read-input [input-path]
-  (->> (s/split-lines (slurp input-path))))
+(def example
+"abcde
+fghij
+klmno
+pqrst
+fguij
+axcye
+wvxyz")
 
-(defn part-one [input-path]
-  (let [{:keys [twos threes]} (->> (read-input input-path)
-                               (map #(frequencies (s/split % #"")))
-                               (map vals)
-                               (reduce (fn [prev n]
-                                         (let [s (set n)]
-                                           (-> prev
-                                               (update :twos (if (contains? s 2) (fnil inc 0) identity))
-                                               (update :threes (if (contains? s 3) (fnil inc 0) identity))))) {}))] 
-  (* twos threes)))
-
-;; (part-one "./src/advent_2018/two/test-input.txt")
-(part-one "./src/advent_2018/two/input.txt")
+(defn part-one
+  ([] (part-one (slurp "./src/2018/day2/input.txt")))
+  ([input]
+   (let [{:keys [twos threes]} (->> (s/split-lines (slurp "./src/2018/day2/input.txt"))
+                                    (map frequencies)
+                                    (map vals)
+                                    (map frequencies)
+                                    (reduce (fn [sum {twos 2 threes 3}]
+                                              (cond-> sum
+                                                twos (update :twos inc)
+                                                threes (update :threes inc)))
+                                            {:twos 0 :threes 0}))]
+        (* twos threes))))
 
 (defn- get-differing-chars [left right]
   (->> (map vector left right)
     (filter (partial apply not=))))
 
-(defn part-two [input-path]
-  (loop [values (read-input input-path)]
+(defn part-two
+  ([] (part-two (slurp "./src/2018/day2/input.txt")))
+  ([input]
+  (loop [values (s/split-lines input)]
     (if-let [[head & tail] values]
       (let [result (->> tail
-                     (reduce (fn [prev n]
-                               (let [differing-chars (get-differing-chars head n)]
-                                 (if (= 1 (count differing-chars))
-                                   {:differing-char (ffirst differing-chars)}
-                                   prev)))))]
+                     (some (fn [word]
+                               (let [differing-chars (get-differing-chars head word)]
+                                 (when (= 1 (count differing-chars))
+                                   differing-chars)))))]
+        (if result
+          (s/replace-first head (re-pattern (-> result ffirst str)) "")
+          (recur tail)))))))
 
-        (if-not (nil? (:differing-char result))
-          (s/replace-first head (re-pattern (str (:differing-char result))) "")
-          (recur tail))))))
-
-(part-two "./src/advent_2018/two/test-input2.txt")
-(part-two "./src/advent_2018/two/input.txt")
-
-; mxhwoglxgeauywfkztndcvjqr
-
+(comment
+  (part-one)
+  (part-two example)
+  (part-two))
