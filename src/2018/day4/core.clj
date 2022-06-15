d8c3d940a3acc5abab460a926e52f3bbbb352073
pcolliander <pontus.colliander@gmail.com>
2022-06-15 10:16:59 +0100

    solved part one

---
+++ src/2018/day4/core.clj
@@ -0,0 +1,82 @@
+(ns advent_2018.four.core
+  (:require [clojure.string :as s]
+            [clojure.set :as cs]))
+
+(def example
+"[1518-11-01 00:00] Guard #10 begins shift
+[1518-11-01 00:05] falls asleep
+[1518-11-01 00:25] wakes up
+[1518-11-01 00:30] falls asleep
+[1518-11-01 00:55] wakes up
+[1518-11-01 23:58] Guard #99 begins shift
+[1518-11-02 00:40] falls asleep
+[1518-11-02 00:50] wakes up
+[1518-11-03 00:05] Guard #10 begins shift
+[1518-11-03 00:24] falls asleep
+[1518-11-03 00:29] wakes up
+[1518-11-04 00:02] Guard #99 begins shift
+[1518-11-04 00:36] falls asleep
+[1518-11-04 00:46] wakes up
+[1518-11-05 00:03] Guard #99 begins shift
+[1518-11-05 00:45] falls asleep
+[1518-11-05 00:55] wakes up
+")
+
+(defn- get-minute-asleep-most [actions]
+  (->> actions
+       (group-by :day)
+       (mapcat (fn [[k v]]
+                 (->> v
+                      (partition 2)
+                      (mapcat (fn [[slept-at wake-up-at]]
+                                (range (:minute slept-at) (:minute wake-up-at)))))))
+       frequencies
+       (sort-by val )
+       last
+       first))
+
+(defn- get-total-minutes-asleep [[guard actions]]
+  [guard (->> actions
+              (partition 2)
+              (reduce (fn [acc [slept-at wake-up-at]]
+                        (+ acc (- (:minute wake-up-at) (:minute slept-at))))
+                      0))])
+
+(defn part-one
+  ([] (part-one (slurp "./src/2018/day4/input.txt")))
+  ([input]
+   (let [lines (->> (s/split-lines input)
+                    sort
+                    (map (fn [line]
+                           (let [[_ _ month day hour minute op] (re-find #"\[(\d+)-(\d+)-(\d+) (\d+):(\d+)\] (.*$)" line)]
+                             {:month month
+                              :day day
+                              :minute (parse-long minute)
+                              :op op}))))
+
+         actions-by-guard (loop [lines lines
+                                 actions-by-guard {}]
+                            (if-let [line (first lines)]
+                              (let [[_ guard] (re-find #"#(\d+)" (:op line))
+                                    actions (->> (next lines) (take-while #(not (re-find #"#(\d+)" (:op %)))))]
+                                (recur (drop-while #(not (re-find #"#(\d+)" (:op %))) (next lines))
+                                       (update actions-by-guard guard concat actions)))
+                              actions-by-guard))
+         guard-ids (keys actions-by-guard)
+         [guard-id _] (->> actions-by-guard
+                           (map get-total-minutes-asleep)
+                           (sort-by second)
+                           last)
+         most-minute-asleep (get-minute-asleep-most (get actions-by-guard guard-id))]
+     (* most-minute-asleep (parse-long guard-id)))))
+
+(defn part-two
+  ([] (part-two (slurp "./src/2018/day4/input.txt")))
+  ([input]
+   ))
+
+(comment
+(part-one example)
+(part-one)
+(part-two example)
+(part-two))
