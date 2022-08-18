f450baa3bd1f7b1d19e892bef41ee64e0770ee29
pcolliander <pontus.colliander@gmail.com>
2022-08-18 08:35:36 +0200

    change to use the refactored version

--- src/2018/day7/core.clj
+++ src/2018/day7/core.clj
@@ -12,92 +12,75 @@ Step D must be finished before step E can begin.
 Step F must be finished before step E can begin.")
 
 (defn parse-line [line]
-  (let [[[_ step before-step]] (re-seq #"Step ([A-Z]).* step ([A-Z])" line)]
-    [(keyword step) (keyword before-step)]))
+  (let [[_ step before-step] (re-matches #"Step ([A-Z]) must be finished before step ([A-Z]) can begin." line) ]
+    [step before-step]))
 
-(defn- parse [lines]
-  (let [lines (s/split-lines lines)]
-    (loop [lines lines
-           requirements {}
-           graph {}]
-      (if (empty? lines)
-        [graph requirements]
-        (let [[before after] (parse-line (first lines))]
-
-          (recur
-            (next lines)
-            (assoc requirements after (conj (requirements after (sorted-set)) before))
-            (assoc graph before (conj (graph before (sorted-set)) after))))))))
-
-(defn- find-start [graph requirements]
-  (->> (keys graph)
-       (remove (fn [k]
-                 (contains? requirements k)))
-       sort))
-
-(defn- ready? [requirements order instruction]
-  (when (cs/superset? (set order) (instruction requirements))
-    instruction))
+(defn- parse [input]
+  (let [lines (map parse-line (s/split-lines input))
+        steps (set (concat (map first lines) (map second lines)))]
+    (merge
+      (into {} (->> steps (map (fn [s]
+                                 [s #{}])) (into {})))
+      (reduce (fn [a [before after]]
+                (assoc a after (conj (a after (sorted-set)) before)))
+              {}
+              lines))))
 
 (defn part-one
   ([] (part-one (slurp "./src/2018/day7/input.txt")))
   ([input]
-   (let [[graph requirements] (parse input)
-         start-nodes (find-start graph requirements)
-         [start-node & r] start-nodes]
-     (loop [instructions (apply sorted-set (concat (start-node graph) r))
-            order [start-node]]
-       (if (empty? instructions)
-         (s/join (map name order))
-         (let [instruction (some #(ready? requirements order %) instructions)
-               new-order (conj order instruction)]
+   (let [requirements (parse input)]
+     (loop [requirements requirements
+            order ""]
+       (if (empty? requirements)
+         (s/join order)
+         (let [next-instruction (->> requirements
+                                     (filter #(empty? (second %)))
+                                     (map first)
+                                     sort
+                                     first)
+               requirements' (into {} (map (fn [[k v]]
+                                             [k (disj v next-instruction)])
+                                           requirements))]
            (recur
-             (cs/difference (apply conj instructions (instruction graph)) (set new-order))
-             new-order)))))))
-
+             (dissoc requirements' next-instruction)
+             (str order next-instruction))))))))
+                 
 (def step-time
   (reduce (fn [a i]
-            (assoc a (keyword (str (char (+ (int \A) i)))) (inc i)))
+            (assoc a (str (char (+ (int \A) i))) (+ 60 (inc i))))
           {}
           (range 0 26)))
 
-(defn- task-time [node]
-  (+ 60 (step-time node)))
-
-(defn- worker-ready? [[_ time-left]]
-  (zero? time-left))
-
 (defn part-two
   ([] (part-two (slurp "./src/2018/day7/input.txt") 5))
-  ([input workn]
-   (let [[graph requirements] (parse input)
-         start-nodes (find-start graph requirements)
-         workers (map #(vector % (task-time %)) start-nodes)]
-     (loop [instructions (sorted-set)
-            order []
-            seconds 0
-            workers workers]
-       (if (and (empty? instructions) (zero? (count workers)))
-         [seconds (s/join (map name order))]
-         (let [workers' (map (fn [[letter time-left]]
-                                [letter (dec time-left)]) workers)
-               finished-workers (filter worker-ready? workers')
-               new-order (concat order (map first finished-workers))
-               new-instructions (apply conj instructions (cs/difference (set (mapcat (fn [[instruction _]]
-                                                                                       (instruction graph)) finished-workers))
-                                                                        (set new-order)))
-               workers'' (reduce (fn [a instruction]
-                                   (let [ready (ready? requirements new-order instruction)]
-                                     (if (and ready (< (count a) workn))
-                                       (conj a [ready (task-time ready)])
-                                       a)))
-                                 (filterv (complement worker-ready?) workers')
-                                 new-instructions)]
-           (recur
-             (cs/difference new-instructions (set (map first workers'')))
-             new-order
-             (inc seconds)
-             workers'')))))))
+  ([input n]
+   (loop [requirements (parse input)
+          seconds 0
+          workers []]
+     (let [{finished false active true} (group-by (comp pos? second)
+                                                  (for [[letter time-left] workers]
+                                                    [letter (dec time-left)]))
+           requirements' (into {}
+                               (map (fn [[k v]]
+                                      [k (apply disj v (map first finished))])
+                                    (apply dissoc requirements (map first finished))))
+           available-requirements (->> requirements'
+                                       (remove (fn [[instruction reqs]]
+                                                 (or
+                                                   (seq reqs)
+                                                   (some #{instruction} (map first active)))))
+                                       (map first))
+           workers' (->> (take (- n (count active)) available-requirements)
+                         (reduce #(conj %1 [%2 (step-time %2)])
+                                 active))]
+
+       (if (empty? requirements')
+         seconds
+         (recur
+           requirements'
+           (inc seconds)
+           workers'))))))
 
 (comment
 (part-one example)
