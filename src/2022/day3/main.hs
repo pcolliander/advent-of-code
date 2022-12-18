5ee19c53b2423aa664ea795c9f980bd26308aed6
pcolliander <pontus.colliander@gmail.com>
2022-12-18 12:19:00 +0000

    solved part one

---
+++ src/2022/day3/main.hs
@@ -0,0 +1,23 @@
+import Data.List
+import qualified Data.Set as Set
+
+getPriority :: Char -> [Int]
+getPriority a = [p | (l, p) <- zip (['a'..'z'] ++ ['A'..'Z']) [1..], a == l] 
+
+compartments :: String -> (String, String)
+compartments l = splitAt ((length l + 1) `div` 2) l
+
+overlapping :: Ord a => ([a], [a]) -> Set.Set a
+overlapping (l1, l2) = Set.intersection(Set.fromList l1) (Set.fromList l2)
+
+part1 :: String -> Int
+part1 input = sum $ concatMap getPriority $ intercalate "" $ map (Set.toList . overlapping . compartments) $ lines input
+
+main = do  
+  example <- readFile "example.txt"
+  input <- readFile "input.txt"
+  print $ part1 example
+  print $ part1 input
+  -- print $ part2 example
+  -- print $ part2 input        
+
