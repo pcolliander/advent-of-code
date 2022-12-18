94b2d32a2e098f5412be8bb9da15c031014fe39e
pcolliander <pontus.colliander@gmail.com>
2022-12-18 14:36:14 +0000

    solved part 2

--- src/2022/day3/main.hs
+++ src/2022/day3/main.hs
@@ -1,4 +1,5 @@
 import Data.List
+import Data.List.Split
 import qualified Data.Set as Set
 
 getPriority :: Char -> Int
@@ -7,17 +8,25 @@ getPriority a = head [p | (l, p) <- zip (['a'..'z'] ++ ['A'..'Z']) [1..], a == l
 compartments :: String -> (String, String)
 compartments l = splitAt ((length l + 1) `div` 2) l
 
-overlapping :: Ord a => ([a], [a]) -> Set.Set a
-overlapping (l1, l2) = Set.intersection(Set.fromList l1) (Set.fromList l2)
+overlapping :: Ord a => ([a], [a]) -> [a]
+overlapping (l1, l2) = Set.toList $ Set.intersection(Set.fromList l1) (Set.fromList l2)
+
+overlapping' :: Ord a => [[a]] -> [a]
+overlapping' (l1: l2: l3:_) = Set.toList $ Set.intersection(Set.fromList l1) $ Set.intersection (Set.fromList l2) (Set.fromList l3)
+
+sumPriorities :: [String] -> Int
+sumPriorities l = sum $ map getPriority $ intercalate "" l
 
 part1 :: String -> Int
-part1 input = sum $ map getPriority $ intercalate "" $ map (Set.toList . overlapping . compartments) $ lines input
+part1 input = sumPriorities $ map (overlapping . compartments) $ lines input
+
+part2 :: String -> Int
+part2 input = sumPriorities $ map overlapping' $ chunksOf 3 $ lines input
 
 main = do  
   example <- readFile "example.txt"
   input <- readFile "input.txt"
   print $ part1 example
   print $ part1 input
-  -- print $ part2 example
-  -- print $ part2 input        
-
+  print $ part2 example
+  print $ part2 input        
