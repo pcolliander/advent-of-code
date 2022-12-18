2ba1795f9c513e970195ad6a12a459245e26722b
pcolliander <pontus.colliander@gmail.com>
2022-12-18 18:51:11 +0000

    solved part 2 in clojure

--- src/2022/day3/core.clj
+++ src/2022/day3/core.clj
@@ -11,45 +11,45 @@ ttgJtRGJQctTZtZT
 CrZsJsPPZsGzwwsLwLmpwMDw
 ")
 
-;; overlapping' :: Ord a => [[a]] -> [a]
-;; overlapping' (l1: l2: l3:_) = Set.toList $ Set.intersection(Set.fromList l1) $ Set.intersection (Set.fromList l2) (Set.fromList l3)
-
-;; part2 :: String -> Int
-;; part2 input = sumPriorities $ map overlapping' $ chunksOf 3 $ lines input
-
 (defn- overlapping [[l1 l2]]
   (into [] (s/intersection (set l1) (set l2))))
+
+(defn- overlapping' [[l1 l2 l3]]
+  (into [] (s/intersection (set l1) (set l2) (set l3))))
 
 (def priorities
   (zipmap (concat (map char (range (int \a) (inc (int \z))))
                   (map char (range (int \A) (inc (int \Z)))))
           (range 1 57)))
 
-(defn- parse [input]
+(defn- parse-compartments [input]
   (->> (string/split-lines input)
        (map (fn [s]
               [(apply str (take (/ (count s) 2) s))
                (apply str (drop (/ (count s) 2) s))]))))
 
-(parse example)
-(map overlapping (parse example))
+(defn- parse-bags [input]
+  (->> (string/split-lines input)
+       (partition 3)))
 
 (defn part-one
-  ([]  (part-one (slurp "./src/2022/day2/input.txt")))
+  ([]  (part-one (slurp "./src/2022/day3/input.txt")))
   ([input]
-   (->> (parse input)
+   (->> (parse-compartments input)
         (mapcat overlapping)
         (map #(get priorities %))
         (reduce +))))
 
 (defn part-two
-  ([]  (part-two (slurp "./src/2022/day2/input.txt")))
+  ([]  (part-two (slurp "./src/2022/day3/input.txt")))
   ([input]
-   (apply + (map play-v2 (parse input)))))
+   (->> (parse-bags input)
+        (mapcat overlapping')
+        (map #(get priorities %))
+        (reduce +))))
 
 (comment
 (part-one example)
 (part-one)
 (part-two example)
 (part-two))
-
