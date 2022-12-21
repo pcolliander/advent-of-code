70efcca6621997c0cbc6858f6e7a5f0bd9c67023
pcolliander <pontus.colliander@gmail.com>
2022-12-21 19:39:12 +0000

    solved part two

--- src/2022/day4/main.hs
+++ src/2022/day4/main.hs
@@ -5,10 +5,13 @@ import qualified Data.Set as Set
 toInt :: [String] -> [Int]
 toInt = map read
 
-overlapping :: Ord a => [[a]] -> Bool
-overlapping [l1, l2] =
+completelyOverlapping :: Ord a => [[a]] -> Bool
+completelyOverlapping [l1, l2] =
   Set.isSubsetOf(Set.fromList l1) (Set.fromList l2)
   || Set.isSubsetOf(Set.fromList l2) (Set.fromList l1)
+
+overlapping :: Ord a => [[a]] -> Bool
+overlapping [l1, l2] = (length $ Set.toList $ Set.intersection(Set.fromList l1) (Set.fromList l2)) /= 0
 
 parse :: String -> [[[Int]]]
 parse input = map (map (toInt . splitOn "-") . splitOn ",") $ lines input
@@ -17,15 +20,15 @@ ranges :: [Int] -> [Int]
 ranges [a,b] = [a..b]
 
 part1 :: String -> Int
-part1 input =  length $ filter id $ map (overlapping . map ranges) $ parse input
+part1 input =  length $ filter id $ map (completelyOverlapping . map ranges) $ parse input
 
--- part2 :: String -> Int
--- part2 input = sumPriorities $ map overlapping' $ chunksOf 3 $ lines input
+part2 :: String -> Int
+part2 input =  length $ filter id $ map (overlapping . map ranges) $ parse input
 
 main = do  
   example <- readFile "example.txt"
   input <- readFile "input.txt"
   print $ part1 example
   print $ part1 input
-  -- print $ part2 example
-  -- print $ part2 input        
+  print $ part2 example
+  print $ part2 input        
