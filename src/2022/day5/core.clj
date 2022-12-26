cb1dce91d5f9fd7718cb5d6aaa025954c48f79e8
pcolliander <pontus.colliander@gmail.com>
2022-12-26 22:19:04 +0000

    solved part two

--- src/2022/day5/core.clj
+++ src/2022/day5/core.clj
@@ -13,20 +13,6 @@ move 3 from 1 to 3
 move 2 from 2 to 1
 move 1 from 1 to 2")
 
-(def example'
-"[P]     [L]         [T]            
-[L]     [M] [G]     [G]     [S]    
-[M]     [Q] [W]     [H] [R] [G]    
-[N]     [F] [M]     [D] [V] [R] [N]
-[W]     [G] [Q] [P] [J] [F] [M] [C]
-[V] [H] [B] [F] [H] [M] [B] [H] [B]
-[B] [Q] [D] [T] [T] [B] [N] [L] [D]
-[H] [M] [N] [Z] [M] [C] [M] [P] [P]
- 1   2   3   4   5   6   7   8   9
-
-move 1 from 2 to 1
-move 3 from 1 to 3")
-
 (defn transpose [m]
   (apply mapv vector m))
 
@@ -47,7 +33,7 @@ move 3 from 1 to 3")
 (defn part-one
   ([] (part-one (slurp "./src/2022/day5/input.txt")))
   ([input]
-   (let [[stacks instructions] (parse' input)
+   (let [[stacks instructions] (parse input)
          with-applied-instructions (reduce (fn [acc [quantity from to]]
                                              (let [items (take quantity (get acc from))]
                                                (-> acc
@@ -60,8 +46,25 @@ move 3 from 1 to 3")
           (map (comp first second))
           (string/join "")))))
 
+(defn part-two
+  ([] (part-two (slurp "./src/2022/day5/input.txt")))
+  ([input]
+   (let [[stacks instructions] (parse input)
+         with-applied-instructions (reduce (fn [acc [quantity from to]]
+                                             (let [items (take quantity (get acc from))]
+                                               (-> acc
+                                                   (update from #(drop quantity %))
+                                                   (update to #(apply conj % (reverse items))))))
+                                           stacks
+                                           instructions)]
+
+     (->> (map vec with-applied-instructions)
+          (sort-by first)
+          (map (comp first second))
+          (string/join "")))))
+
 (comment
   (part-one example)
   (part-one)
-  (part-two)
+  (part-two example)
   (part-two))
