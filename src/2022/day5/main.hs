5445f3cd763310d2882671ba0fcadf30891d3717
pcolliander <pontus.colliander@gmail.com>
2022-12-28 23:31:47 +0000

    solved part one haskell

--- src/2022/day5/main.hs
+++ src/2022/day5/main.hs
@@ -1,26 +1,43 @@
 import Data.List
 import Data.List.Split
 import qualified Data.Set as Set
-import Data.Char (isSpace)
 import Text.Regex.Posix
 
-trim :: String -> String
-trim = f . f
-   where f = reverse . dropWhile isSpace
+toInt :: [String] -> [Int]
+toInt = map read
 
 parseStacks stacks = map (filter (/= "")) $ transpose $ map (map (\ (_:_:match:_) -> match) . (\x -> x =~ regex :: [[String]])) $ init $ lines stacks
   where regex =  "(\\[(\\w)\\]| ( ) ) ?"
 
-parse input = let (stacks:instructions:_) = splitOn "\n\n" input
-               in (parseStacks stacks, map trim $ lines instructions)
+parseInstruction :: String -> [Int]
+parseInstruction instruction = concatMap (toInt . tail) (instruction =~ regex :: [[String]])
+  where regex = "move ([0-9]*) from ([0-9]) to ([0-9])"
 
-part1 input = stacks
+parse input = (parseStacks stacks, map parseInstruction $ lines instructions)
+  where (stacks:instructions:_) = splitOn "\n\n" input
+
+nth :: Int -> [(Int, [String])] -> (Int, [String])
+nth 1 xs = head xs
+nth n (x:xs) = nth (n-1) xs
+
+applyInstruction :: [Int] -> [(Int, [String])] -> (Int, [String]) -> (Int, [String])
+applyInstruction instruction acc numberedStack
+  | nr == from = (nr, drop quantity stack)
+  | nr == to   = (nr, reverse items ++ stack)
+  | otherwise  = numberedStack
+  where (quantity: from: to:_) = instruction
+        items = take quantity $ snd $ nth from acc
+        (nr, stack) = numberedStack
+
+part1 input = intercalate "" $ map (head . snd) finalStacks
   where (stacks,instructions) = parse input
+        numberedStacks = zip [1..] stacks
+        finalStacks = foldl (\acc instruction -> map (applyInstruction instruction acc) acc) numberedStacks instructions 
 
 main = do  
   example <- readFile "example.txt"
   input <- readFile "input.txt"
   print $ part1 example
-  -- print $ part1 input
+  print $ part1 input
   -- print $ part2 example
   -- print $ part2 input        
