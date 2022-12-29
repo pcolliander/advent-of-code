32ab2c4aff0657abe50208d5fce472fd3e2f2e93
pcolliander <pontus.colliander@gmail.com>
2022-12-29 20:01:29 +0000

    part two

--- src/2022/day6/main.hs
+++ src/2022/day6/main.hs
@@ -8,17 +8,27 @@ parse input = filter (/= "") $ splitOn "" input
 
 startOfPackage :: Int -> [String] -> Int
 startOfPackage count full@(_:xs)
-  | 4 == packageLength =  count + 4
+  | 4 == packageLength = count + 4
   | otherwise = startOfPackage (count + 1) xs
   where packageLength = length $ Set.fromList(take 4 full)
+
+startOfMessage :: Int -> [String] -> Int
+startOfMessage count full@(_:xs)
+  | 14 == packageLength = count + 14
+  | otherwise = startOfMessage (count + 1) xs
+  where packageLength = length $ Set.fromList(take 14 full)
 
 part1 :: String -> Int
 part1 input = startOfPackage 0 $ parse input
 
+part2 :: String -> Int
+part2 input = startOfMessage 0 $ parse input
+
 main = do  
   example <- readFile "example.txt"
+  example2 <- readFile "example2.txt"
   input <- readFile "input.txt"
   print $ part1 example
   print $ part1 input
-  -- print $ part2 example
-  -- print $ part2 input        
+  print $ part2 example2
+  print $ part2 input        
