299ff65c749b4c885227f9a43999324570080f02
Pontus Colliander <pontus.colliander@gmail.com>
2023-01-06 21:28:09 +0100

    2022/day7 (#24)
    * test input working
    
    * fixed bug, folder are not unique but depend on the containing folder
    
    * part two
    
    * simplified a bit

---
+++ src/2022/day7/core.clj
@@ -0,0 +1,98 @@
+(ns advent-of-code-2022.core
+   (:require [clojure.string :as string]
+             [clojure.set :as s]))
+
+(def example
+"$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k")
+
+(defn parse [input] 
+  (-> input
+    (string/replace #"\n" " ")
+    (string/split #" " )))
+                    
+(defn filepath [dir]
+  (str "/" (string/join "/" (next dir))))
+
+(defn- process [n]
+  (loop [commands n
+         filesystem {}
+         dir []]
+    (let [[command tail] (split-with #(not= % "$") (next commands))]
+      (if (seq command)
+        (case (first command)
+          "cd" (case (second command)
+                 ".." (recur tail filesystem (pop dir))
+                 "/"  (recur tail filesystem ["/"])
+                 (recur tail filesystem (conj dir (second command))))
+          "ls"   (recur tail (assoc filesystem (filepath dir)
+                                    (->> (next command) (partition 2) (map (fn [[f s]]
+                                                                             (if (= f "dir")
+                                                                               [f (filepath (conj dir s))]
+                                                                               [f s]))))) dir))
+        filesystem))))
+
+(defn- size [filesystem [f s]]
+  (if (= f "dir")
+    (->> (get filesystem s) (map (partial size filesystem)) flatten)
+    (parse-long f)))
+
+(defn- filesystem-with-size [filesystem]
+  (reduce-kv (fn [acc k v]
+               (assoc acc k (->> v
+                                 (map (partial size filesystem))
+                                 flatten 
+                                 (reduce +))))
+             {} filesystem))
+
+(defn part-one
+  ([] (part-one (slurp "./src/2022/day7/input.txt")))
+  ([input]
+   (let [filesystem (-> input parse process)]
+     (->> (filesystem-with-size filesystem)
+          vals
+          (filter (partial >= 100000))
+          (reduce +)))))
+
+(defn part-two
+  ([] (part-two (slurp "./src/2022/day7/input.txt")))
+  ([input]
+   (let [filesystem-with-sz (-> input parse process filesystem-with-size)
+         total-used (apply max (vals filesystem-with-sz))
+         unused-space (- 70000000 total-used)
+         needed-space (- 30000000 unused-space)]
+
+     (->> filesystem-with-sz
+          (sort-by (comp second vec))
+          (drop-while (fn [[f s]]
+                        (<= s needed-space)))
+          (first)
+          (second)))))
+
+(comment
+  (part-one example)
+  (part-one)
+  (part-two example)
+  (part-two))
+
