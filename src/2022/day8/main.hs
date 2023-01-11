3e25bbc49b34dcb51bf5ada9680bee05fec79e52
pcolliander <pontus.colliander@gmail.com>
2023-01-11 18:21:19 +0000

    part two

--- src/2022/day8/main.hs
+++ src/2022/day8/main.hs
@@ -17,6 +17,12 @@ parse input = map (map toInt . filter (/= "") . splitOn "") $  words input
 visible :: Int -> [Int] -> Bool
 visible tree trees = all (==True) $ scanl (\acc tree' -> tree > tree') True trees
 
+score :: Int -> [Int] -> Int -> Int
+score tree [] result = result
+score tree (x:xs) result = if tree <= x
+                              then result + 1
+                              else score tree xs result + 1
+
 part1 :: String -> Int
 part1 input = length [tree | row <- [0..length rows - 1],
                       column     <- [0..length (head rows) - 1],
@@ -28,11 +34,22 @@ part1 input = length [tree | row <- [0..length rows - 1],
                       visible tree after || visible tree before || visible tree under || visible tree above]
   where rows = parse input
 
+part2 :: String -> Int
+part2 input =  maximum [scenicScore | row <- [0..length rows - 1],
+                      column     <- [0..length (head rows) - 1],
+                      let tree = (rows !! row) !! column,
+                      let treeLineH = rows !! row,
+                      let treeLineV = transpose rows !! column,
+                      let (before, after) = lineOfSight treeLineH column,
+                      let (under, above) = lineOfSight treeLineV row,
+                      let scenicScore = score tree before 0 * score tree after 0 * score tree under 0 * score tree above 0]
+  where rows = parse input
+
 main = do  
   example <- readFile "example.txt"
   input <- readFile "input.txt"
   print $ part1 example
   print $ part1 input
-  -- print $ part2 example
-  -- print $ part2 input        
+  print $ part2 example
+  print $ part2 input        
 
