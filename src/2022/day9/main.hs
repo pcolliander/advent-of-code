87d368ce47c2bf032d14c3bbf34da8619a3be78a
pcolliander <pontus.colliander@gmail.com>
2023-02-05 22:23:55 +0000

    remove some duplication

--- src/2022/day9/main.hs
+++ src/2022/day9/main.hs
@@ -13,23 +13,17 @@ parse input =  map (toInstruction . words) $ filter (/= "") $ splitOn "\n" input
 
 isAdjacent (x1, y1) (x2, y2) = (2 > abs (x2 - x1)) && (2 > abs (y2 - y1))
 
-move step (head, tail) = (head', if isAdjacent head' tail then tail else head)
-  where (x, y) = head
-        head'
-          | step == "R" = (x + 1, y)
-          | step == "L" = (x - 1, y)
-          | step == "U" = (x, y + 1)
-          | step == "D" = (x, y - 1)
-          | otherwise = (x, y)
-
-move' step (x, y)  
+move step (x, y)  
   | step == "R" = (x + 1, y)
   | step == "L" = (x - 1, y)
   | step == "U" = (x, y + 1)
   | step == "D" = (x, y - 1)
   | otherwise = (x, y)    
 
-motion state (l, quantity) = foldl (\(knot, visited) step -> let knot' = move step knot in (knot', snd knot': visited )) state steps
+motion state (l, quantity) = foldl (\((head,tail), visited) step -> let head' = move step head
+                                                                        tail' = if isAdjacent head' tail then tail else head
+                                                                   in ((head', tail'), tail': visited ))
+                                                                   state steps
   where steps = replicate quantity l
 
 determineMove t@(x1, y1) h@(x2, y2)
@@ -50,7 +44,7 @@ straightMove (x1, y1) point
   | isAdjacent (x1 + 1, y1)  point = (x1 + 1, y1)
   | otherwise = (x1, y1)
 
-motion' state (l, quantity) = foldl (\(head:tail, visited) step -> let head' = move' step head
+motion' state (l, quantity) = foldl (\(head:tail, visited) step -> let head' = move step head
                                                                        knot = foldl (\acc n -> if isAdjacent (last acc) n then acc ++ [n] else acc ++ [determineMove n (last acc)]) [head'] tail
                                                                     in (knot, last knot:visited))
                                                                     state steps
